872dea2a19445add17e70e5c74314c5210dec5fd
MAHANMALPORU734 <mahan.navidu@gmail.com>
2021-02-20 20:05:00 +0530

    Initial Commit

---
+++ M.M.M.N.Malporu-COBSCCOMP182P-016-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M.M.M.N.Malporu-COBSCCOMP182P-016-Calculator
+//
+//  Created by Mahan Navidu on 2021-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ M.M.M.N.Malporu-COBSCCOMP182P-016-CalculatorTests/M_M_M_N_Malporu_COBSCCOMP182P_016_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  M_M_M_N_Malporu_COBSCCOMP182P_016_CalculatorTests.swift
+//  M.M.M.N.Malporu-COBSCCOMP182P-016-CalculatorTests
+//
+//  Created by Mahan Navidu on 2021-02-20.
+//
+
+import XCTest
+@testable import M_M_M_N_Malporu_COBSCCOMP182P_016_Calculator
+
+class M_M_M_N_Malporu_COBSCCOMP182P_016_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
